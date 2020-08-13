707ff0a3361fd7fbc49ff2219dc8996bc3da6758
RogerioKlein <64874964+RogerioKlein@users.noreply.github.com>
2020-08-12 23:02:55 -0300

    Codigo Main

---
+++ Caronex/src/entidades/Pessoa.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author roger
+ */
+@Entity
+@Table(name = "cad_pessoa")
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pessoa")
+    private int id_pessoa;
+
+    @Column(name = "identidade")
+    private String identidade;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "telefone")
+    private String telefone;
+    
+    @Column(name = "idade")
+    private int idade;
+
+    @Column(name = "id_responsavel")
+    private int id_responsavel;
+
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public String getIdentidade() {
+        return identidade;
+    }
+
+    public void setIdentidade(String identidade) {
+        this.identidade = identidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getId_responsavel() {
+        return id_responsavel;
+    }
+
+    public void setId_responsavel(int id_responsavel) {
+        this.id_responsavel = id_responsavel;
+    }
+    
+    
+}
